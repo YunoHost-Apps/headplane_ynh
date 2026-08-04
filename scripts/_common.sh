109e1ce443ec0dd1c9f17b34657fc36148247e1e
tituspijean <tituspijean@yunohost.org>
2026-08-04 22:41:05 +0200

    fix(oidc): wrong key indent and callback url

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -51,7 +51,7 @@ setup_dex() {
 	dex_install_dir="$(ynh_app_setting_get --app $dex --key install_dir)"
 	dex_domain="$(ynh_app_setting_get --app $dex --key domain)"
 	dex_path="$(ynh_app_setting_get --app $dex --key path)"
-	oidc_callback="https://$domain${path%/}/oidc/callback"
+	oidc_callback="https://$domain${path%/}/admin/oidc/callback"
 
 	# If the API key needs updating (exclude Headscale requirement in CI context)
 	if [[ -z "${api_key:-}" || "$(date +%s)" -gt "${api_key_expires:-0}" ]]; then
